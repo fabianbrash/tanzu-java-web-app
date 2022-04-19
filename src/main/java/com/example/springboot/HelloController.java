ecf937ff06893d396984fe131f4d91bad437aac4
Fabian Brash <fabianbrash@gmail.com>
2022-04-19 14:27:28 -0400

    changed name in workload yaml

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from the feature branch!";
+		return "Greetings from the feature branch + Tanzu!";
 	}
 
 }

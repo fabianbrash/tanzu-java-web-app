9aa82a4d4535146e6151644239c4c5eb78a4dce6
Fabian Brash <fabianbrash@gmail.com>
2022-04-19 15:08:02 -0400

    still testing..

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from the feature branch + Tanzu!", controller.index());
+        assertEquals("Greetings from the feature branch!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from the feature branch + Tanzu!"));
+            .andExpect(content().string("Greetings from the feature branch!"));
     }
 }
